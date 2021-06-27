30d4fc49e78ad996a35361e544e8bf7687e20738
Figueiredo0512 <39870694+Figueiredo0512@users.noreply.github.com>
2021-06-27 20:16:01 -0300

    Add files via upload

---
+++ Segundo ex com classe/Segundo ex com classe/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Segundo_ex_com_classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler nome e salario de dois funcionarios.
+            //Depois mostrar o salario medio dos funcionarios.
+
+            Funcionario a, b;
+            a = new Funcionario();
+            b = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionario: ");
+            Console.Write("Nome: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            a.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Dados do segundo funcionario: ");
+            Console.Write("Nome: ");
+            b.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            b.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (a.Salario + b.Salario) / 2;
+
+            Console.WriteLine("Salario medio = " + media.ToString("F2"));
+
+        }
+    }
+}
---
+++ While_exercitando/While_exercitando/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace While_exercitando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+            while (numero >= 0)
+            {
+                Console.Write("Digite um numero para saber sua raiz quadrada: ");
+                numero = double.Parse(Console.ReadLine());
+                Console.WriteLine("Raiz quadrada de " + numero + " = " + Math.Sqrt(numero).ToString("F3"));
+            }
+            Console.WriteLine("Programa finalizado. Numero negativo.");
+        }
+    }
+}
+
