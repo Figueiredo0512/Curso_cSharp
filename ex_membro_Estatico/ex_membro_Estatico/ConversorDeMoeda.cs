f9640a1e25d91e75e2dd374236b63e8369611330
Figueiredo0512 <39870694+Figueiredo0512@users.noreply.github.com>
2021-06-30 19:37:42 -0300

    Add files via upload

---
+++ ex_membro_Estatico/ex_membro_Estatico/ConversorDeMoeda.cs
@@ -0,0 +1,9 @@
+﻿namespace ex_membro_Estatico {
+    class ConversorDeMoeda {
+
+
+        public static double Conversao(double dolar, double qtd) {
+            return dolar * qtd;
+        }
+    }
+}
---
+++ ex_membro_Estatico/ex_membro_Estatico/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex_membro_Estatico {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Qual a cotacao do dolar ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dolares vc vai comprar? ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = ConversorDeMoeda.Conversao(dolar, qtd);
+            Console.WriteLine("Valor a se pago em R$: " + total.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
