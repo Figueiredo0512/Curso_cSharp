afb6c68698317be7f2db41b30e71b4720f55503c
Figueiredo0512 <39870694+Figueiredo0512@users.noreply.github.com>
2021-06-30 19:02:34 -0300

    Add files via upload

---
+++ ex3_classe_obj/ex3_classe_obj/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ex3_classe_obj {
+    class Aluno {
+
+        public string Nome;
+        public double Nota_1;
+        public double Nota_2;
+        public double Nota_3;
+        public double Media = 0.0;
+        public double Resta;
+
+        public void Situacao() {
+            Media = (Nota_1 + Nota_2 + Nota_3) / 3;
+        }
+
+
+        public override string ToString() {
+            if (Media < 50.00) {
+                Resta = 50 - Media;
+                return "Reprovado!\r\nFaltaram " + Resta.ToString("F2", CultureInfo.InvariantCulture) + " Pontos";
+            }
+            else {
+                return "Nota Final: " + Media.ToString("F2", CultureInfo.InvariantCulture) + "\r\nAprovado!";
+            }
+        }
+    }
+}
---
+++ ex3_classe_obj/ex3_classe_obj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex3_classe_obj {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno x = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            
+            x.Nome = Console.ReadLine();
+            x.Nota_1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            x.Nota_2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            x.Nota_3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            x.Situacao();
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
