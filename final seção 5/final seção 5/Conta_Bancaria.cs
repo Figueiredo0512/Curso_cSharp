baeb9d6f42490eec803e54b7c0ccc17909fd0f09
Figueiredo0512 <39870694+Figueiredo0512@users.noreply.github.com>
2021-07-19 20:31:42 -0300

    Add files via upload

---
+++ final seção 5/final seção 5/Conta_Bancaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace final_seção_5 {
+    class Conta_Bancaria {
+
+        private string _nome;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta_Bancaria() {
+
+        }
+
+        public Conta_Bancaria(string nome, int numero) {
+            _nome = nome;
+            Numero = numero;
+        }
+
+
+        public Conta_Bancaria(string nome, int numero, double saldo) {
+            _nome = nome;
+            Numero = numero;
+            Saldo = saldo;
+        }
+
+        public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNumero(int numero) {
+            Numero = numero;
+        }
+
+        public int GetNumero() {
+            return Numero;
+        }
+
+        private void SetSaldo(double saldo) {
+            Saldo = saldo;
+        }
+
+        public double GetSaldo() {
+            return Saldo;
+        }
+
+        public void deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void saque(double quantia) {
+            Saldo -= quantia - 5.00;
+        }
+    }
+}
---
+++ final seção 5/final seção 5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace final_seção_5 {
+    class Program {
+        static void Main(string[] args) {
+            string nome;
+            int numero;
+            double saldo, quantia;
+            string yon;
+
+            Conta_Bancaria conta = new Conta_Bancaria();
+
+            Console.Write("Entre com o numero da conta bancaria: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular da conta: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Havera saldo iniciarl? S/n: ");
+            yon = Console.ReadLine();
+
+
+            if (yon == "S" || yon == "s") {
+                Console.WriteLine("Entre com o saldo inicial: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta_Bancaria(nome, numero, saldo);
+            }
+
+            else {
+                conta = new Conta_Bancaria(nome, numero);
+            }
+
+            Console.WriteLine("\n\rDados da conta: ");
+            Console.WriteLine("Nome: " + conta.GetNome() + ", Numero da conta: " + conta.GetNumero() + " Saldo: R$" + conta.GetSaldo());
+
+            Console.WriteLine("Deposito: ");
+            quantia = double.Parse(Console.ReadLine());
+            conta.deposito(quantia);
+
+            Console.WriteLine("\n\rDados da conta: ");
+            Console.WriteLine("Nome: " + conta.GetNome() + ", Numero da conta: " + conta.GetNumero() + " Saldo: R$" + conta.GetSaldo());
+
+            Console.WriteLine("Saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            conta.saque(quantia);
+
+            Console.WriteLine("\n\rDados da conta: ");
+            Console.WriteLine("Nome: " + conta.GetNome() + ", Numero da conta: " + conta.GetNumero() + " Saldo: R$" + conta.GetSaldo());
+        
+        }
+    }
+}
